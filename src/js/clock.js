9ae419055f738108c5f7be4b0ffb83d78f6bb507
Damixn31 <damianol29@hotmail.com>
2024-05-11 15:06:35 -0300

    index

---
+++ src/js/clock.js
@@ -0,0 +1,27 @@
+const time = document.getElementById("time");
+const date = document.getElementById("date");
+
+const mountNames = [
+  "January",
+  "Febrery",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const interval = setInterval(() => {
+  const local = new Date();
+  let day = local.getDay(),
+    month = local.getMonth(),
+    year = local.getFullYear();
+
+  time.innerHTML = local.toLocaleTimeString();
+  date.innerHTML = `${day} ${mountNames[month]} ${year}`;
+}, 1000);
