dd15b6fa86c2c05ca3dd797de1f88e29d60e0b33
Manuel David Ramirez <Manuyagami95@gmail.com>
2024-05-18 19:55:52 -0300

    Corrección del script clock

--- src/js/clock.js
+++ src/js/clock.js
@@ -18,7 +18,7 @@ const mountNames = [
 
 const interval = setInterval(() => {
   const local = new Date();
-  let day = local.getDay(),
+  let day = local.getDate(),
     month = local.getMonth(),
     year = local.getFullYear();
 
