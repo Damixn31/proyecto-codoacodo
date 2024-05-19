4712fba8389b08e1360d9e31353a7219d6e10221
Manuel David Ramirez <Manuyagami95@gmail.com>
2024-05-18 21:16:56 -0300

    validación mejorada

--- src/js/validation.js
+++ src/js/validation.js
@@ -2,21 +2,25 @@ const nombre = document.getElementById("name");
 const email = document.getElementById("email");
 const motivo = document.getElementById("reason");
 const mensaje = document.getElementById("message");
-const precauciones = document.getElementById("warnings")
+const errores = document.getElementById("warnings")
 
 form.addEventListener("submit", e => {
     e.preventDefault();
+    let errores = [];
     if (!nombre.value) {
-        alert("El nombre es necesario");
+        errores.push("El nombre es necesario");
     }
     if (!email.value) {
-        alert("El email es necesario");
+        errores.push("El email es necesario");
     }
     if (!motivo.value) {
-        alert("Seleccione su motivo de su mensaje")
+        errores.push("Seleccione su motivo de su mensaje")
     }
     if (!mensaje.value) {
-        alert("El mensaje no puede estar vacio");
+        errores.push("El mensaje no puede estar vacio");
+
+    } if (errores.length > 0) {
+        alert(errores.join("\n"))
     } else {
         alert("Mensaje enviado, Gracias por contactarse con nosotros")
     }
